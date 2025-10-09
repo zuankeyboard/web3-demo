197f1996e685108d0eee85ad62f25cda07ae8919
lumos <522438869@qq.com>
2025-10-09 14:50:41 +0800

    feat(web3): 添加web3页面和相关配置
    添加web3页面组件，包含地址显示功能
    更新next配置以支持ant-design相关包转译
    添加web3相关依赖包
    优化全局样式格式

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["@ant-design", "antd", "rc-util", "rc-pagination", "rc-picker", "rc-input"],
 }
 
 module.exports = nextConfig
---
+++ pages/web3.tsx
@@ -0,0 +1,7 @@
+import { Address } from "@ant-design/web3";
+
+export default function Web3() {
+    return (
+        <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
+    );
+}
