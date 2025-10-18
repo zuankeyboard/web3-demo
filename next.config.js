167427840091a4eea3d0b7bbeb742ad0f263feee
lumos <522438869@qq.com>
2025-10-18 21:08:16 +0800

    build: 添加rc-table和rc-util依赖并更新next配置
    添加rc-table和rc-util作为项目依赖，同时更新next.config.js以包含新的transpilePackages和实验性esmExternals配置

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["@ant-design", "antd", "rc-util", "rc-pagination", "rc-picker", "rc-input"],
+  transpilePackages: [
+    "@ant-design", 
+    "antd", 
+    "rc-util", 
+    "rc-pagination", 
+    "rc-picker", 
+    "rc-input",
+    "rc-table",
+    "rc-tree",
+    "rc-select",
+    "rc-tooltip",
+    "rc-dropdown"
+  ],
+  experimental: {
+    esmExternals: 'loose'
+  }
 }
 
 module.exports = nextConfig
